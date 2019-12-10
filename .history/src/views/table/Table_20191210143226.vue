144c9f1e6706fe75b10a4d6c588ea969efd2983b
qianxuebing <2625318262@qq.com>
2019-12-10 14:45:56 +0800

    add table

---
+++ .history/src/views/table/Table_20191210143226.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <el-form :inline="true" :model="formInline">
+      <el-form-item label="姓名">
+         <el-input v-model="formInline.name" placeholder="请输入姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="联系方式">
+         <el-input v-model="formInline.telephone" placeholder="请输入联系方式"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSearch">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table>
+
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      formInline: {
+        name: '',
+        telephone: ''
+      }
+    }
+  },
+  methods: {
+    onSearch () {
+      if (this.formInline.name === undefined && this.formInline.telephone === undefined) {
+        this.$message({
+          message: '请输入查询条件',
+          type: 'error'
+        })
+        return false
+      } else {
+        this.$message({
+          message: '查询成功',
+          type: 'success'
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ .history/src/views/table/Table_20191210144347.vue
@@ -0,0 +1,79 @@
+<template>
+  <div>
+    <el-form :inline="true" :model="formInline">
+      <el-form-item label="姓名">
+         <el-input v-model="formInline.name" placeholder="请输入姓名"></el-input>
+      </el-form-item>
+      <el-form-item label="联系方式">
+         <el-input v-model="formInline.telephone" placeholder="请输入联系方式"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSearch">查询</el-button>
+      </el-form-item>
+    </el-form>
+    <el-table :data="tableData" style="width: 100%" border>
+      <el-table-column
+        prop="date"
+        label="日期"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="name"
+        label="姓名"
+        width="80">
+      </el-table-column>
+      <el-table-column
+        prop="tel"
+        label="电话"
+        width="100">
+      </el-table-column>
+      <el-table-column
+        prop="address"
+        label="地址">
+      </el-table-column>
+       <el-table-column
+        prop="operation"
+        label="操作">
+        <template slot-scope="scope">
+          <!-- <el-button @click="handleClick(scope.row)" type="primary" size="small">编辑</el-button>
+          <el-button @click="handleClick(scope.row)" type="text" size="small">删除</el-button> -->
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+import tableData from './tableData.js'
+export default {
+  data () {
+    return {
+      tableData: tableData,
+      formInline: {
+        name: '',
+        telephone: ''
+      }
+    }
+  },
+  methods: {
+    onSearch () {
+      if (this.formInline.name === '' && this.formInline.telephone === '') {
+        this.$message({
+          message: '请输入查询条件',
+          type: 'error'
+        })
+        return false
+      } else {
+        this.$message({
+          message: '查询成功',
+          type: 'success'
+        })
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
